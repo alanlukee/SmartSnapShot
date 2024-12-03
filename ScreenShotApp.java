cb94e1cf86b4c0dfd7728f10cd83518b8abecb1e
Alan Luke <amenu4cse20205@am.students.amrita.edu>
2024-12-03 17:33:38 +0530

    changed icon, changed background, commented "add interval", automatic minimization.

--- ScreenShotApp.java
+++ ScreenShotApp.java
@@ -2,6 +2,8 @@ package com.SmartSnapShot;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class ScreenShotApp {
@@ -11,23 +13,23 @@ public class ScreenShotApp {
 		JFrame frame = new JFrame("Smart SnapShot");
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(460,150);
+		frame.setSize(460,200);
 		frame.setLayout(null);
-		frame.getContentPane().setBackground(Color.white);
+		frame.getContentPane().setBackground(Color.BLACK);
 		
 		
 		// start button 
 		int startButtonHeight = 85;
 		int startButtonWidth = 80;
 		
-		ImageIcon startIcon = new ImageIcon("C:\\Users\\2021603\\eclipse-workspace\\SmartSnapShot\\src\\assets\\camera_icon.jpg");
+		ImageIcon startIcon = new ImageIcon("C:\\Users\\2021603\\eclipse-workspace\\SmartSnapShot\\src\\assets\\snapshot_icon.png");
 		
 		Image scaledStartImage = startIcon.getImage().getScaledInstance(startButtonWidth, startButtonHeight, Image.SCALE_SMOOTH);
 		ImageIcon scaledStartIcon = new ImageIcon(scaledStartImage);
 		
 		//active icon for the start button.
 		
-		ImageIcon activeIcon = new ImageIcon("C:\\Users\\2021603\\eclipse-workspace\\SmartSnapShot\\src\\assets\\camera_active.png");
+		ImageIcon activeIcon = new ImageIcon("C:\\Users\\2021603\\eclipse-workspace\\SmartSnapShot\\src\\assets\\snapshot_active.png");
 		Image scaledActiveImage = activeIcon.getImage().getScaledInstance(startButtonWidth, startButtonHeight,  Image.SCALE_SMOOTH);
 		ImageIcon scaledActiveIcon = new ImageIcon(scaledActiveImage);
 		
@@ -43,34 +45,41 @@ public class ScreenShotApp {
 		int endButtonWidth = 75;
 		int endButtonHeight = 84;
 		
-		ImageIcon stopIcon = new ImageIcon("C:\\Users\\2021603\\eclipse-workspace\\SmartSnapShot\\src\\assets\\stop.png");
+		ImageIcon stopIcon = new ImageIcon("C:\\Users\\2021603\\eclipse-workspace\\SmartSnapShot\\src\\assets\\snapshot_stop.png");
 		Image scaledStopImage = stopIcon.getImage().getScaledInstance(endButtonWidth, endButtonHeight,Image.SCALE_SMOOTH);
 		ImageIcon scaledStopIcon = new ImageIcon(scaledStopImage);
 		
 		JButton endButton = new JButton(scaledStopIcon);
-		endButton.setBounds(180,20,endButtonWidth,endButtonHeight);
+		endButton.setBounds(190,20,endButtonWidth,endButtonHeight);
 		endButton.setBorderPainted(false);
 		endButton.setToolTipText("Stop screen capture");
 		frame.add(endButton);
 		
 		// timer Button
 
-		int timerButtonWidth =  60;
-		int timerButtonHeight = 65;
+		int timerButtonWidth =  75;
+		int timerButtonHeight = 80;
 		
-		ImageIcon timerIcon = new ImageIcon("C:\\Users\\2021603\\eclipse-workspace\\SmartSnapShot\\src\\assets\\timer.jpg");
+		ImageIcon timerIcon = new ImageIcon("C:\\Users\\2021603\\eclipse-workspace\\SmartSnapShot\\src\\assets\\timer.png");
 		Image timerImage= timerIcon.getImage().getScaledInstance(timerButtonWidth, timerButtonHeight, Image.SCALE_SMOOTH);
 		ImageIcon scaledTimerIcon = new ImageIcon(timerImage);
 		
 		JButton timerButton = new JButton(scaledTimerIcon);
-		timerButton.setBounds(310,30,timerButtonWidth,timerButtonHeight);
+		timerButton.setBounds(310,18,timerButtonWidth,timerButtonHeight);
 		timerButton.setBorderPainted(false);
 		timerButton.setToolTipText("Snapshot interval");
 		frame.add(timerButton);
+		
+		
 	
 		//time interval list
-		String[] timeIntervals = {"1 seconds","2 seconds","3 seconds","4 seconds","5 seconds"};
-		JList<String> timeList = new JList<>(timeIntervals);
+		 DefaultListModel<String> listModel = new DefaultListModel<String>();
+			
+			for(int i = 1; i <= 10;i++) {
+				listModel.addElement(i+" second"+ (i >1 ? "s":""));
+			}
+			
+			JList<String> timeList = new JList<>(listModel);
 				
 		timeList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);		
 		JScrollPane scrollPane = new JScrollPane(timeList);
@@ -95,12 +104,37 @@ public class ScreenShotApp {
 					}
 				});
 		
+//		// button to dynamically add new intervals
+//		
+//		JButton addButton = new JButton("+");
+//		addButton.setBounds(350,110,50,50);
+//		frame.add(addButton);
+//		
+//		addButton.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				String newInterval = JOptionPane.showInputDialog(frame,"Enter new Interval(in seconds)");
+//				if(newInterval != null && !newInterval.trim().isEmpty()) {
+//					try {
+//						
+//						int interval = Integer.parseInt(newInterval.trim());
+//						listModel.addElement(interval + "second"+(interval > 1?"s" :""));
+//						
+//					} catch(NumberFormatException ex) {
+//						
+//						JOptionPane.showMessageDialog(frame,"Please enter a valid number!","Invalid input",JOptionPane.ERROR_MESSAGE);
+//					}
+//				}
+//			}
+//		});
+		
 		
 		//adding action listener to start button to change its icon.
 		startButton.addActionListener(e->
 		{
 					startButton.setIcon(scaledActiveIcon);
 					System.out.println("Snapshot started");
+					//frame.setState(Frame.ICONIFIED);
 		});
 		
 		//adding action listener to stop button---> to restore the start button icon.
@@ -111,9 +145,8 @@ public class ScreenShotApp {
 		System.out.println("Snapshot stopped");
 		});
 		
-		
-		//frame.setLocation(1050, 650);
-		frame.setLocationRelativeTo(null);
+		frame.setLocation(1050, 650);
+		//frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 
 	}
