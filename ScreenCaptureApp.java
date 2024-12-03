b370ee4fe2b41092bed221e59877464f646dd6dc
Alan Luke <amenu4cse20205@am.students.amrita.edu>
2024-12-03 22:39:23 +0530

    Add files via upload

--- ScreenCaptureApp.java
+++ ScreenCaptureApp.java
@@ -1,68 +1,203 @@
 package com.smartSnapShot;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 
 public class ScreenCaptureApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("Smart SnapShot");
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(450,150);
+		frame.setSize(460,150);
 		frame.setLayout(null);
 		frame.getContentPane().setBackground(Color.BLACK);
 		
-		int startButtonWidth = 80;  
-        int startButtonHeight = 75;
-
-       
-        ImageIcon originalIcon = new ImageIcon("/Users/alanluke/Desktop/LukZ/QuEST/snapshot_icon.png"); 
-        Image scaledImage = originalIcon.getImage().getScaledInstance(startButtonWidth, startButtonHeight, Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon = new ImageIcon(scaledImage);
-
-        
-		//start button
-		JButton startButton = new JButton(scaledIcon);
-		startButton.setBounds(50, 20, startButtonWidth, startButtonWidth);
-		startButton.setToolTipText("Start screen capture");
+		
+		// start button 
+		int startButtonHeight = 85;
+		int startButtonWidth = 80;
+		
+		ImageIcon startIcon = new ImageIcon("/Users/alanluke/Desktop/LukZ/QuEST/snapshot_icon.png");
+		
+		Image scaledStartImage = startIcon.getImage().getScaledInstance(startButtonWidth, startButtonHeight, Image.SCALE_SMOOTH);
+		ImageIcon scaledStartIcon = new ImageIcon(scaledStartImage);
+		
+		//active icon for the start button.
+		
+		ImageIcon activeIcon = new ImageIcon("/Users/alanluke/Desktop/LukZ/QuEST/snapshot_active.png");
+		Image scaledActiveImage = activeIcon.getImage().getScaledInstance(startButtonWidth, startButtonHeight,  Image.SCALE_SMOOTH);
+		ImageIcon scaledActiveIcon = new ImageIcon(scaledActiveImage);
+		
+		JButton startButton = new JButton(scaledStartIcon);
+		startButton.setBounds(50,20,startButtonWidth,startButtonHeight);
+		startButton.setBorderPainted(false);
+		startButton.setToolTipText("start screen capture");
 		frame.add(startButton);
 		
-		int endButtonWidth = 60;  
-        int endButtonHeight = 65;
-		
-        ImageIcon originalIcon_2 = new ImageIcon("/Users/alanluke/Desktop/LukZ/QuEST/stop_icon.jpg"); 
-        Image scaledImage_2 = originalIcon_2.getImage().getScaledInstance(endButtonWidth, endButtonHeight, Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon_2 = new ImageIcon(scaledImage_2);
 		
 		//end button
-		JButton endButton = new JButton(scaledIcon_2);
-		endButton.setBounds(200, 28, endButtonWidth, endButtonHeight);
-	
+		
+		int endButtonWidth = 75;
+		int endButtonHeight = 84;
+		
+		ImageIcon stopIcon = new ImageIcon("/Users/alanluke/Desktop/LukZ/QuEST/snapshot_stop.png");
+		Image scaledStopImage = stopIcon.getImage().getScaledInstance(endButtonWidth, endButtonHeight,Image.SCALE_SMOOTH);
+		ImageIcon scaledStopIcon = new ImageIcon(scaledStopImage);
+		
+		JButton endButton = new JButton(scaledStopIcon);
+		endButton.setBounds(190,20,endButtonWidth,endButtonHeight);
+		endButton.setBorderPainted(false);
 		endButton.setToolTipText("Stop screen capture");
+		endButton.setEnabled(false);
 		frame.add(endButton);
 		
-		int timerButtonWidth = 60;  
-        int timerButtonHeight = 65;
+		// timer Button
+
+		int timerButtonWidth =  75;
+		int timerButtonHeight = 80;
 		
-        ImageIcon originalIcon_3 = new ImageIcon("/Users/alanluke/Desktop/LukZ/QuEST/timer.png"); 
-        Image scaledImage_3 = originalIcon_3.getImage().getScaledInstance(endButtonWidth, endButtonHeight, Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon_3 = new ImageIcon(scaledImage_3);
+		ImageIcon timerIcon = new ImageIcon("/Users/alanluke/Desktop/LukZ/QuEST/timer.png");
+		Image timerImage= timerIcon.getImage().getScaledInstance(timerButtonWidth, timerButtonHeight, Image.SCALE_SMOOTH);
+		ImageIcon scaledTimerIcon = new ImageIcon(timerImage);
 		
-		//end button
-		JButton timerButton = new JButton(scaledIcon_3);
-		timerButton.setBounds(320, 25, timerButtonWidth, timerButtonHeight);
-	
-		timerButton.setToolTipText("snapshot intervals");
+		JButton timerButton = new JButton(scaledTimerIcon);
+		timerButton.setBounds(310,18,timerButtonWidth,timerButtonHeight);
+		timerButton.setBorderPainted(false);
+		timerButton.setToolTipText("Snapshot interval");
 		frame.add(timerButton);
 		
 		
+	
+		//time interval list
+		 DefaultListModel<String> listModel = new DefaultListModel<String>();
+			
+			for(int i = 1; i <= 10;i++) {
+				listModel.addElement(i+" second"+ (i >1 ? "s":""));
+			}
+			
+			JList<String> timeList = new JList<>(listModel);
+				
+		timeList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);		
+		JScrollPane scrollPane = new JScrollPane(timeList);
+						
+		JPopupMenu popupMenu = new JPopupMenu();
+		popupMenu.add(scrollPane);
+				
+		timerButton.addActionListener(e ->{
+				popupMenu.show(timerButton,60,0);
+				});
+				
+		// add list selection listener to handle the selection of time intervals	
+		timeList.addListSelectionListener(event ->{
+			if(! event.getValueIsAdjusting()) {
+				String selectedInterval = timeList.getSelectedValue();
+						
+				if(selectedInterval != null) {
+					timerButton.setToolTipText("selected interval:"+ selectedInterval);
+							//close the popup menu
+					popupMenu.setVisible(false);
+						}
+					}
+				});
+		
+//		// button to dynamically add new intervals
+//		
+//		JButton addButton = new JButton("+");
+//		addButton.setBounds(350,110,50,50);
+//		frame.add(addButton);
+//		
+//		addButton.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				String newInterval = JOptionPane.showInputDialog(frame,"Enter new Interval(in seconds)");
+//				if(newInterval != null && !newInterval.trim().isEmpty()) {
+//					try {
+//						
+//						int interval = Integer.parseInt(newInterval.trim());
+//						listModel.addElement(interval + "second"+(interval > 1?"s" :""));
+//						
+//					} catch(NumberFormatException ex) {
+//						
+//						JOptionPane.showMessageDialog(frame,"Please enter a valid number!","Invalid input",JOptionPane.ERROR_MESSAGE);
+//					}
+//				}
+//			}
+//		});
 		
 		
+		//adding action listener to start button to change its icon.
+		startButton.addActionListener(e->
+		{
+					startButton.setIcon(scaledActiveIcon);
+					System.out.println("Snapshot started");
+					frame.setState(Frame.ICONIFIED);
+					frame.setIconImage(scaledActiveImage);
+					endButton.setEnabled(true);
+					
+					 try {
+			                // Capture the screen
+			                Robot robot = new Robot();
+			                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			                Rectangle screenRect = new Rectangle(screenSize);
+
+			                BufferedImage screenshot = robot.createScreenCapture(screenRect);
+			                System.out.println("Screen captured successfully!");
+			                
+			                //Set up a temporary preview frame
+			                JFrame screenshotFrame = new JFrame("Screenshot Preview");
+
+			                // Shrink the screenshot for preview
+			                int previewWidth = (int) (screenSize.width * 0.5); // 50% of screen width
+			                int previewHeight = (int) (screenSize.height * 0.5); // 50% of screen height
+			                Image scaledScreenshot = screenshot.getScaledInstance(previewWidth, previewHeight, Image.SCALE_SMOOTH);
+
+			                // Create a JLabel to hold the scaled image
+			                ImageIcon screenshotIcon = new ImageIcon(scaledScreenshot);
+			                JLabel screenshotLabel = new JLabel(screenshotIcon);
+
+			                // Add the JLabel to the frame
+			                screenshotFrame.add(screenshotLabel);
+			                screenshotFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			                screenshotFrame.pack(); // Adjust the frame size to fit the scaled image
+			                screenshotFrame.setLocationRelativeTo(null); // Center the frame
+			                screenshotFrame.setVisible(true);
+
+
+			            } catch (Exception ex) {
+			                ex.printStackTrace();
+			                JOptionPane.showMessageDialog(frame, "Failed to capture screen: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			            }
+			
+		});
 		
+		//adding action listener to stop button---> to restore the start button icon.
+		
+		endButton.addActionListener(e->
+		{
+		startButton.setIcon(scaledStartIcon);
+		System.out.println("Snapshot stopped");
+		frame.setIconImage(scaledStartImage);
+		endButton.setEnabled(false);
+		frame.dispose();
+		
+		});
+		
+		//frame.setLocation(1050, 650);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
-		
 
 	}
 
 }
+
+
+
+
