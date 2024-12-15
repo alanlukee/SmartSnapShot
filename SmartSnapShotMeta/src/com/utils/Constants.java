1d1be36f53b92792bc5667af9a0fd172bb348f1b
Alan Luke <amenu4cse20205@am.students.amrita.edu>
2024-12-15 20:34:41 +0530

    Add files via upload

---
+++ SmartSnapShotMeta/src/com/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.utils;
+
+public class Constants {
+
+	public static final String DEFAULT_PDF_NAME = "fileName.pdf";
+
+	public static final String ACTIVE_ICON_PATH = "src\\main\\java\\assets\\snapshot_active.png";
+	public static final String CAMERA_ICON_PATH = "src\\main\\java\\assets\\snapshot_icon.png";
+	public static final String STOP_CAMERA_ICON_PATH = "src\\main\\java\\assets\\snapshot_stop.png";
+	public static final String TIMER_ICON_PATH = "src\\main\\java\\assets\\timer.png";
+
+	public static final String[] TIMER_INTERVAL_OPTIONS = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+	public static final String[] TIMER_OPTIONS = { "User demand", "Time interval" };
+
+}
